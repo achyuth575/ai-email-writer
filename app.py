e4fb00de5175f473f6e0e769dfc444890a597bc1
Achyuth Chowdary <achyuthtech@gmail.com>
2026-02-13 10:46:19 +0530

    Deployment setup

--- app.py
+++ app.py
@@ -231,7 +231,8 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
 
 import smtplib
 from email.mime.text import MIMEText
